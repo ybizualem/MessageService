7310718238bcfe57b40911cc07f5ca01d19d7250
ybizualem <144256402+ybizualem@users.noreply.github.com>
2024-04-23 13:59:19 -0400

    Update Message

--- src/main/java/com/example/messageservice/Entries/Message.java
+++ src/main/java/com/example/messageservice/Entries/Message.java
@@ -3,7 +3,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-//"hello test"
 @Entity
 public class Message {
     @Id
