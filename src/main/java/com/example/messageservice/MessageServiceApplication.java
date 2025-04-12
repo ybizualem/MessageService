46d5d28f8da61959654db95c4934d461f61ede96
Yeabsira Bizualem <144256402+ybizualem@users.noreply.github.com>
2025-04-12 13:03:45 -0400

    Update MessageServiceApplication.java

--- src/main/java/com/example/messageservice/MessageServiceApplication.java
+++ src/main/java/com/example/messageservice/MessageServiceApplication.java
@@ -13,18 +13,4 @@ public class MessageServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MessageServiceApplication.class, args);
     }
-//    @Bean
-//    public CommandLineRunner commandLineRunner() {
-//        String app_id = "1784918";
-//        String key = "2d21e5ca31cdaee85787";
-//        String secret = "e7ff0b60aff91316a91e";
-//        String cluster = "us2";
-//        return args -> {
-//            Pusher pusher = new Pusher(app_id, key, secret);
-//            pusher.setCluster(cluster);
-//            pusher.setEncrypted(true);
-//            pusher.trigger("my-channel", "my-event", Collections.singletonMap("message", "hello world"));
-//        };
-//    }
-
 }
